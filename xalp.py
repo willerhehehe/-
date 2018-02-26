8aef5a0f03f3ebccad2d19341efdebc01573738c
willerhehehe <32084498+willerhehehe@users.noreply.github.com>
2018-02-26 18:11:27 +0800

    Update xalp.py

--- xalp.py
+++ xalp.py
@@ -1,6 +1,18 @@
 #coding:utf-8
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pymysql
+config = {
+	'host':'127.0.0.1',
+	'port':3306,
+	'user':'root',
+	'password':'*******'#数据库密码,
+	'db':'loupan',
+	'charset':'utf8mb4',
+	'cursorclass':pymysql.cursors.DictCursor,
+}
+
+
 
 xalp_dict={}
 url='http://xa.fang.lianjia.com/loupan/nht1pg1'
@@ -17,8 +29,26 @@ def robot(url):
 		xalp_dict[name]['乡']=i.findAll('div',{'class':'resblock-location'})[0].i.next_sibling.next_sibling.text
 		xalp_dict[name]['地址']=i.find('div',{'class':'resblock-location'}).a.text
 		xalp_dict[name]['是否在售']=i.find('span',{'class':'sale-status'}).text
+		price = xalp_dict[name]['价格']
+		distract = xalp_dict[name]['区']
+		town = xalp_dict[name]['乡']
+		address = xalp_dict[name]['地址']
+		status = xalp_dict[name]['是否在售']
+		cursor = connection.cursor()
+		print("楼盘:%s,价格:%s,区:%s,乡:%s,地址:%s,状态:%s"%(name,xalp_dict[name]['价格'],xalp_dict[name]['区'],xalp_dict[name]['乡'],xalp_dict[name]['地址'],xalp_dict[name]['是否在售']))
+		print(type(name),type(price),type(address))
+		#sql = '''INSERT INTO xian(楼盘,价格,区,乡,地址,状态) VALUES (name,xalp_dict[name]['价格'],xalp_dict[name]['区'],xalp_dict[name]['乡'],xalp_dict[name]['地址'],xalp_dict[name]['是否在售'])'''
+		sql = "INSERT INTO xian(楼盘,价格,区,乡,地址,状态) VALUES ('{}','{}','{}','{}','{}','{}')".format(name,price,distract,town,address,status)
+		try:
+			cursor.execute(sql)
+			connection.commit()
+		except Exception as e:
+			print(e)
+			connection.rollback()
 
 if __name__ == '__main__':
+	#Connect to the database
+	connection = pymysql.connect(**config)
 	html = urlopen(url)
 	bsObj = BeautifulSoup(html,'html.parser')
 	total_pages = int(bsObj.find('div',{'class':'resblock-have-find'}).span.next_sibling.next_sibling.text)
@@ -32,7 +62,7 @@ if __name__ == '__main__':
 		if i>=67:
 			break
 	print('END')
-	
+	connection.close()
 		
 
 '''
